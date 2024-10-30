a223e427116998043a46d3479f974ff99c3d7a6a
Wesley Peres <wesley.peres@bluepex.com>
2024-10-30 20:38:08 -0300

    Second Commit

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,7 +5,12 @@ function Tasks(props) {
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button className="bg-slate-400 text-left w-full text-white p-2 rounded-md">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+              task.isCompleted && "line-through"
+            }`}
+          >
             {task.title}
           </button>
           <button className="bg-slate-400 text-white p-2 rounded-md">
