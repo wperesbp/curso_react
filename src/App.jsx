7dc5fea4d491b8a6cde513c91910ce856ac45718
Wesley Peres <wesley.peres@bluepex.com>
2024-10-31 18:33:13 -0300

    Add delete task button

--- src/App.jsx
+++ src/App.jsx
@@ -36,13 +36,22 @@ function App() {
     setTasks(newTasks);
   }
 
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id != taskId);
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
         <AddTask />
       </div>
     </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,12 +1,12 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, Trash2Icon } from "lucide-react";
 
-function Tasks(props) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
-            onClick={() => props.onTaskClick(task.id)}
+            onClick={() => onTaskClick(task.id)}
             className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
               task.isCompleted && "line-through"
             }`}
@@ -15,6 +15,12 @@ function Tasks(props) {
           </button>
           <button className="bg-slate-400 text-white p-2 rounded-md">
             <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => onDeleteTaskClick(task.id)}
+            className="bg-slate-400 text-white p-2 rounded-md"
+          >
+            <Trash2Icon />
           </button>
         </li>
       ))}
